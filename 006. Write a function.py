d1a173a9310ab00625291d98070ca3ba71672b93
Marius Gheorghe <32913797+tksmagh@users.noreply.github.com>
2019-05-19 16:33:27 -0400

    Update 006. Write a function.py

--- 006. Write a function.py
+++ 006. Write a function.py
@@ -1,5 +1,16 @@
 # Problem: https://www.hackerrank.com/challenges/write-a-function/problem
 
 
-def is_leap(y):
-    return y % 4 == 0 and (y % 100 != 0 or y % 400 == 0)
+def is_leap(year):
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+year = int(input())
+print(is_leap(year))
+
+#def is_leap(year):    
+#    if year%4==0:
+#        return True
+#    elif year%100==0:
+#        return False
+#    elif year%400==0:
+#        return True
